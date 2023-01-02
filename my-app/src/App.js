a236ded2b337875dfcbed5f0c52aab90bcd3e80b
MiguelBrockmann <miguelbrockmann@gmail.com>
2023-01-02 14:43:49 +0100

    errror handling with error throw

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -6,22 +6,33 @@ import './App.css';
 function App() {
     const [movies,setMovies] = useState([]);
     const [isLoading,setIsloading] = useState(false);
+    const [error,setError] = useState(null);//null weil objetct
+
 
  async function fetchMovie() {
      setIsloading(true);
-    const response = await  fetch('https://swapi.dev/api/films');
-     const data= await  response.json();
-              const transformedMovies = data.results.map(movieData => {
-                  return {
-                      id: movieData.episode_id,
-                      title: movieData.title,
-                      openingText: movieData.opening_crawl,
-                      releaseDate: movieData.release_date
-                  };
-              })
-             setMovies(transformedMovies);
-              setIsloading(false);
-  }
+     setError(null);//clear old errors
+     try{
+         const response = await  fetch('https://swapi.dev/api/films2');
+         if (!response.ok){ // weil 404 error kein richtiger error ist behandeln wir das so. würde nur error schmeisen wenn mit diesem 404 error was gemacht wird
+             throw new Error('something went wrong2!');//weil die api keine rror message gibt machen wir es so, landen dann im catch.
+         }
+         const data= await  response.json();
+         const transformedMovies = data.results.map(movieData => {
+             return {
+                 id: movieData.episode_id,
+                 title: movieData.title,
+                 openingText: movieData.opening_crawl,
+                 releaseDate: movieData.release_date
+             };
+         })
+         setMovies(transformedMovies);
+         setIsloading(false);
+     }catch (error){ // landen hier wenn error accured
+            setError(error.message)// wollen die message machen das damit patkrisch zum string ohne da sgehts nicht
+     }
+     setIsloading(false)//am ende soll nicht mehr geloadtet werden egal ob fetch erfolgreich ode rnicht
+ }
 
   return (
     <React.Fragment>
@@ -30,7 +41,8 @@ function App() {
       </section>
       <section>
           {!isLoading && movies.length >0 && <MoviesList movies={movies} /> }
-          {isLoading && movies.length === 0 && <p> no movie found</p>}
+          {!isLoading && movies.length === 0 && !error && <p> no movie found</p>}
+          {!isLoading && error && <p>{error}</p>}
           {isLoading && <p> loading</p>}
       </section>
     </React.Fragment>
